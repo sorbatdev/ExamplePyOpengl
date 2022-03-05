8d185979d46fb7ed6a6d991d379821e710114b3d
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-06 01:37:18 +0300

    More callbacks and some documentation

--- window.py
+++ window.py
@@ -8,17 +8,39 @@ EVENT_WINDOW_RESIZE         = 3
 EVENT_WINDOW_FBUFFER_RESIZE = 4
 EVENT_WINDOW_CLOSE          = 5
 
+EVENT_MOUSE_MOVE    = 6
+EVENT_MOUSE_PRESS  = 7
+EVENT_MOUSE_RELEASE = 8
+
+# this can probably be a normal list
 m_Callbacks = {
     EVENT_KEY_PRESS   : [],
     EVENT_KEY_RELEASE : [],
     EVENT_KEY_REPEAT  : [],
     EVENT_WINDOW_RESIZE : [],
     EVENT_WINDOW_FBUFFER_RESIZE: [],
-    EVENT_WINDOW_CLOSE : []
+    EVENT_WINDOW_CLOSE : [],
+    EVENT_MOUSE_MOVE : [],
+    EVENT_MOUSE_PRESS : [],
+    EVENT_MOUSE_RELEASE : []
 }
 
-def AddToCallback(cb_type, cb):
-    m_Callbacks.get(cb_type).append(cb)
+def AddToCallback(cb_type : int, callback_function):
+    """
+    Examples of callbacks are below:\n
+
+    ==          cb_type         ||         Callback body     ==\n
+    \n
+    EVENT_KEY_PRESS             ->     def OnKeyPressed(key) \n
+    EVENT_KEY_RELEASE           ->     def OnKeyReleased(key)\n
+    EVENT_WINDOW_RESIZE         ->     def OnWindowResize(width, height)\n
+    EVENT_WINDOW_FBUFFER_RESIZE ->     def OnFramebufferResize(width, height)\n
+    EVENT_WINDOW_CLOSE          ->     def OnWindowClose()\n
+    EVENT_MOUSE_MOVE            ->     def OnMouseMoved(mouse_x, mouse_y)\n
+    EVENT_MOUSE_PRESS           ->     def OnMouseButtonPress(mouse_x, mouse_y, mouse_button)\n
+    EVENT_MOUSE_RELEASE         ->     def OnMouseButtonsRelease(mouse_x, mouse_y, mouse_button)\n
+    """
+    m_Callbacks.get(cb_type).append(callback_function)
 
 
 # Window class for a single instance of a glfwWindow
@@ -37,27 +59,33 @@ class Window:
             glfwInit()
             Window.kIsInitialized = True
         
-        self.window = glfwCreateWindow(self.width, self.height, self.title, None, None)
+        self.m_Window = glfwCreateWindow(self.width, self.height, self.title, None, None)
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, self.version_major)
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, self.version_minor)
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
 
-        glfwMakeContextCurrent(self.window)
+        glfwMakeContextCurrent(self.m_Window)
 
-        glfwSetKeyCallback(self.window, Window.OnKeyEvent)
-        glfwSetWindowCloseCallback(self.window, Window.OnWindowClose)
+        glfwSetKeyCallback(self.m_Window, Window.OnKeyEvent)
 
-        return self.window # returns the _GLFWwindow, but this is optional
+        glfwSetWindowSizeCallback(self.m_Window, Window.OnWindowResize)
+        glfwSetFramebufferSizeCallback(self.m_Window, Window.OnWindowFramebufferResize)
+        glfwSetWindowCloseCallback(self.m_Window, Window.OnWindowClose)
+
+        glfwSetCursorPosCallback(self.m_Window, Window.OnMouseMove)
+        glfwSetMouseButtonCallback(self.m_Window, Window.OnMouseButton)
+
+        return self.m_Window # returns the _GLFWwindow, but this is optional
 
     def Update(self):
-        glfwSwapBuffers(self.window)
+        glfwSwapBuffers(self.m_Window)
         glfwPollEvents()
 
     def ShouldClose(self):
-        return glfwWindowShouldClose(self.window)
+        return glfwWindowShouldClose(self.m_Window)
 
     def Terminate(self):
-        glfwDestroyWindow(self.window)
+        glfwDestroyWindow(self.m_Window)
         glfwTerminate()
 
     @staticmethod
@@ -70,6 +98,28 @@ class Window:
                 callback(key)
 
     @staticmethod
-    def OnWindowClose(window):
+    def OnWindowResize(_, width, height):
+        for callback in m_Callbacks.get(EVENT_WINDOW_RESIZE):
+            callback()
+
+    @staticmethod
+    def OnWindowFramebufferResize(_, width, height):
+        for callback in m_Callbacks.get(EVENT_WINDOW_FBUFFER_RESIZE):
+            callback()
+
+    @staticmethod
+    def OnWindowClose(_):
         for callback in m_Callbacks.get(EVENT_WINDOW_CLOSE):
             callback()
+
+    @staticmethod
+    def OnMouseMove(_, x, y):
+        for callback in m_Callbacks.get(EVENT_WINDOW_CLOSE):
+            callback(x, y)
+
+    @staticmethod
+    def OnMouseButton(window, button, action, mods):
+        x, y = glfwGetCursorPos(window)
+        if action == GLFW_PRESS:
+            for callback in m_Callbacks.get(EVENT_MOUSE_PRESSED):
+                callback(x, y, button)
